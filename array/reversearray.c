a22a86106afeee1a44494c22158a2e9610d64fa2
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-08-01 10:12:48 +0530

    Update reversearray.c

--- array/reversearray.c
+++ array/reversearray.c
@@ -1,25 +1,26 @@
 /*
 1 2 3 4 5  
-A B C D E
+A B C D E  ----this are addresses of arr[0]  arr[1]  arr[2]  arr[3]  arr[4] 
 
 step 1:
-B>A
+B>A         &arr[1]>&arr[0]
 swap(A,B)
 B A C D E
 step2:
-C>A
+C>A        &arr[2]>&arr[1]
 swap(A,C)
 B C A D E
 step3:
-D>A
+D>A        &arr[3]>&arr[2]
 swap(A,D)
 B C D A E
 step 4:
-E>A
+E>A        &arr[4]>&arr[3]
 swap(A,E)
 B C D E A
 2 3 4 5 1
- 
+            1st time 4 iteration,second time 3 iteration next 2times......
+            total-(no of ieration)=n-step-1(if step initialized with 0)
  5 4 3 2 1 
  E D C B A
 */
@@ -54,6 +55,7 @@ int main()
 			}
 		}
 	}
+	printf("Reverse of the array :\n");
 	for(i=0;i<n;i++)
 	{
 		printf("%d ",arr[i]);
